a6d7aad2df341b2ed00bc5ffdb7e969bb39a5b26
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-03-24 15:33:16 -0500

    Create SetUser function

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,40 @@
 package config
 
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
+
 type Config struct {
 	DbURL string `json:"db_url"`
 	CurrentUsername string `json:"current_user_name,omitempty"`
+}
+
+func READ() (Config, error) {
+	newStruct := Config{}
+	homeDirectory, err := os.UserHomeDir()
+	if err != nil {
+		return newStruct, err
+	}
+
+	fileName := ".gatorconfig.json"
+	filePath := filepath.Join(homeDirectory, fileName)
+
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return newStruct, err
+	}
+
+	err = json.Unmarshal(data, &newStruct)
+	if err != nil {
+		return newStruct, err
+	}
+
+	return newStruct, nil
+}
+
+func (c *Config) SetUser(username string) error {
+	c.CurrentUsername = username
+	return write(*c)
 }
